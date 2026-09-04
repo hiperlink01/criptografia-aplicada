08534d45a9814f96a55976d4049d4fedc5ea8303
Oliver Melo <contato_limaverde@protonmail.com>
2026-09-04 12:13:56 -0300

    removendo testes de inverso multiplicativo de euclides_estendido_test.py

--- src/euclides_estendido_test.py
+++ src/euclides_estendido_test.py
@@ -1,15 +1,8 @@
-from euclides_estendido import euclides_estendido, inverso_multiplicativo
+from euclides_estendido import euclides_estendido 
+from aritmetica_modular import NumeroModular as nMod
 
 mdc, x, y = euclides_estendido(7, 3)
 
 print("MDC:", mdc)
 print("x:", x)
 print("y:", y)
-
-inverso = inverso_multiplicativo(3, 7)
-
-print("inverso multiplicativo:", inverso)
-
-inverso = inverso_multiplicativo(6, 9)
-
-print("inverso de 6 modulo 9:", inverso)
